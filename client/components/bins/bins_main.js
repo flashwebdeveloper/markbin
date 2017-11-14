b835285c5c3b2d0bedd7792f90d8c9eef628bfc3
flashwebdeveloper <wasim.kabir92@gmail.com>
2017-11-15 00:36:48 +0530

    Markdown viewer

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { Bins } from '../../../imports/collections/bins';
 import BinsEditor from './bins_editor';
+import BinsViewer from './bins_viewer';
 
 class BinsMain extends Component {
   render() {
@@ -9,7 +10,8 @@ class BinsMain extends Component {
 
     return (
       <div>
-        <BinsEditor bin={this.props.bin }/>
+        <BinsEditor bin={this.props.bin } />
+        <BinsViewer bin={this.props.bin} />
       </div>
     );
   }
