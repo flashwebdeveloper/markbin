879fce3006c92bf60807f286411c41af7dca933c
flashwebdeveloper <wasim.kabir92@gmail.com>
2017-11-14 21:38:18 +0530

    Routing with react-router and react-router-dom (Slight change due to react-router v4

--- client/main.js
+++ client/main.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, IndexRoute, browserHistory } from 'react-router-dom';
 
 import App from './components/app';
 import { Bins } from '../imports/collections/bins';
 
+const routes = (
+  <BrowserRouter history={browserHistory}>
+    <Route path="/" component={App}></Route>
+  </BrowserRouter>
+);
+
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.querySelector('.render-target'));
+  ReactDOM.render(routes, document.querySelector('.render-target'));
 });
