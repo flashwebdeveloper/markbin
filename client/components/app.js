e9670f7e666c963731ba4d2508e278b365d18e5d
flashwebdeveloper <wasim.kabir92@gmail.com>
2017-11-14 20:57:09 +0530

    Fetching the bins with subscription

--- client/components/app.js
+++ client/components/app.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './header.js';
+import BinsList from './bins/bins_list';
 
 export default () => {
   return (
     <div>
       <Header />
+      <BinsList />
     </div>
   );
 }
