695e99d6dc4406ff55804c64314099d0cfbf5910
flashwebdeveloper <wasim.kabir92@gmail.com>
2017-11-14 23:47:22 +0530

    Accessing the correct bin from the bins_main.js Part:2

--- client/components/bins/bins_main.js
+++ client/components/bins/bins_main.js
@@ -4,12 +4,16 @@ import { Bins } from '../../../imports/collections/bins';
 
 class BinsMain extends Component {
   render() {
+    console.log(this.props.bin);
     return (
       <div>BinsMain</div>
     );
   }
 }
 
-export default withTracker(() => {
+export default withTracker((props) => {
+  const { binId } = props.params;
+  Meteor.subscribe('bins');
 
+  return{ bin: Bins.findOne(binId) };
 })(BinsMain);
